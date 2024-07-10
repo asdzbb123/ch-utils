f6fa718d445cc0027432be389a92e1c646adbca5
赵彬镔 <8607099+lhkjqd@user.noreply.gitee.com>
2024-07-10 14:24:10 +0800

    新增判断用户设备

--- src/main/is/index.ts
+++ src/main/is/index.ts
@@ -2,7 +2,6 @@
  * 判断是否为 xxx 相关工具函数
  */
 
-
 /**
  * 检查一个值是否为空。
  * @param value - 需要检查的值.
@@ -10,4 +9,26 @@
  */
 export function isEmpty(value: any): boolean {
     return value == null || (typeof value === 'string' && value.trim() === '') || (Array.isArray(value) && value.length === 0) || (typeof value === 'object' && Object.keys(value).length === 0);
+}
+
+type DeviceType = 'AppleSystem' | 'Mobile' | 'PC';
+
+/**
+ * 判断当前用户设备类型
+ * @returns {DeviceType} 设备的类型，可以是“AppleSystem”、“Mobile”或“PC”。
+ */
+export function isDeviceType(): DeviceType {
+    const userAgent = navigator.userAgent;
+
+    const hasTouchScreen = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+    if (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(userAgent)) {
+        return 'Mobile';
+    } else if (/macintosh|mac os x/.test(userAgent)) {
+        return 'AppleSystem';
+    } else if (hasTouchScreen) {
+        return 'Mobile';
+    } else {
+        return 'PC';
+    }
 }
