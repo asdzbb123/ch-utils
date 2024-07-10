0580ba147635b9e25192c40252f3f594ee7879d9
赵彬镔 <8607099+lhkjqd@user.noreply.gitee.com>
2024-07-10 10:03:59 +0800

    修改转换排序,可查看注释文档

--- src/main/transformation/index.ts
+++ src/main/transformation/index.ts
@@ -2,13 +2,6 @@
  * 转换工具函数
  */
 
-/**
- * 将面积在不同单位之间转换。
- * @param value - 面积的数量。
- * @param fromUnit - 源单位，可以是 "m2"、"km2" 或 "mu"。
- * @param toUnit - 目标单位，可以是 "m2"、"km2" 或 "mu"。
- * @returns 转换后的面积。
- */
 const CONVERSION_RATES = {
     "m2": { "m2": 1, "km2": 0.000001, "mu": 0.0015 },
     "km2": { "m2": 1000000, "km2": 1, "mu": 1500 },
@@ -16,6 +9,13 @@ const CONVERSION_RATES = {
 } as const;
 // as const 对象只读
 export type Unit = keyof typeof CONVERSION_RATES;
+/**
+ * 将面积在不同单位之间转换。
+ * @param value - 面积的数量。
+ * @param fromUnit - 源单位，可以是 "m2"、"km2" 或 "mu"。
+ * @param toUnit - 目标单位，可以是 "m2"、"km2" 或 "mu"。
+ * @returns 转换后的面积。
+ */
 export function convertArea(value: number, fromUnit: Unit, toUnit: Unit): number {
     if (!(fromUnit in CONVERSION_RATES) || !(toUnit in CONVERSION_RATES[fromUnit])) {
         throw new Error("Invalid unit provided.");
